5ace923ebe27732ccd8b12cce9956798ea6585e7
borg <borg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-09-12 10:53:22 +0000

    in compare():
    -vorläufige Behandlung externer ids implementiert, ist noch zu überarbeiten
    -Übergabe von Häufigkeiten in der Struktur (für EM-Algorithmus)
    -weiterhin implementiert: Stringvergleich (jarowinkler oder benutzerdefiniert),
     Kölner Phonetik
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recordlinkage@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/compare.r
+++ trunk/R/compare.r
@@ -13,14 +13,19 @@
 #               die als Argumente zwei Zeilen von dataset hat und TRUE zurückgibt,
 #               falls diese als Paar in die Vergleichsmuster übernommen werden
 #               sollen, ansonsten FALSE.
-#  ids          TRUE, falls die erste Spalte ids enthält. Ansonsten werden die 
-#               Zeilen von dataset durchnummeriert.                                     
+#  ids          TRUE, falls die erste Spalte ids enthält oder ein Vektor der Länge
+#               nrow(dataset) mit ids. Standardmäßig werden die 
+#               Zeilen durchnummeriert.                                     
 #  phonetic     Entweder ein Vektor mit Indizes von Spalten, die vor der Bildung der 
 #               Vergleichsmuster in einen phonetischen Code transformiert werden
-#               sollen, oder TRUE, falls dies für alle Felder geschehen soll.
+#               sollen, oder TRUE, falls dies für alle Felder geschehen soll, oder
+#               FALSE, falls keine Phonetik verwendet wird.
 #  strcmp       Entweder ein Vektor mit Indizes von Feldern, für die bei der
 #               Bildung der Vergleichsmuster eine Stringmetrik verwendet werden
 #               soll, oder TRUE, falls dies für alle Felder geschehen soll.
+#
+#  strcmpfun    Funktion, die als Argumente zwei Strings hat und einen 
+#               Vergleichswert im Interval [0,1] zurückgibt. Standard: Jaro-Winkler
 
 
 # Rückgabe: Stringvergleich gibt bei "NA" 0 zurück!
@@ -51,52 +56,69 @@
 
 
 
-compare <- function(dataset, blockfld=FALSE, blockfun=NULL,ids=1:nrow(dataset),phonetic=FALSE,strcmp=FALSE)
+
+compare <- function(dataset, blockfld=FALSE, blockfun=NULL,ids=1:nrow(dataset),phonetic=FALSE,strcmp=FALSE,strcmpfun=F)
 {
-    ids # Argument wird erst initialisiert, wenn es gebraucht wird,
-        # deshalb muss er hier auftauchen, bevor dataset transponiert wird 
-    # format conversions
     ndata=nrow(dataset)
-    dataset=as.matrix(dataset)
-    dataset[dataset==""]=NA
-    
-    if (!isFALSE(phonetic))
+    if (isTRUE(ids))
     {
-        if (isTRUE(phonetic))
+        # ids in Tabelle gegeben: Nehme erste Spalte aus den Daten heraus
+        ext_ids=dataset[,1]
+        ids=1:ndata
+        dataset=as.matrix(dataset[,-1])
+        if (!isFALSE(blockfld))
+            blockfld=lapply(blockfld,"-",1)
+        if (is.numeric(phonetic))
+            phonetic=phonetic-1
+        if (!isFALSE(strcmp))
+            strcmp=strcmp-1
+    } else if (length(ids)==ndata) # use external ids via argument
+    {
+        ext_ids=ids
+        ids=1:ndata    
+    } else 
+    {
+        ext_ids=1:ndata
+        ids=ext_ids
+    }
+
+    dataset[dataset==""]=NA
+        
+    if (!isFALSE(phonetic)) # true, if phonetic is T or not a logical value
+    {
+        if (isTRUE(phonetic)) # true, if phonetic is a logical value and T
         {    
             dataset=pho_h(dataset)
-        } else
+        } else # phonetic is not a logical value
         dataset[,phonetic]=pho_h(dataset[,phonetic])
     }
     
-    if (!is.function(blockfun))
-        blockfun=jarowinkler
+    if (!is.function(strcmpfun))
+        strcmpfun=jarowinkler
     
-    if (!identical(blockfld,F))
+    if (!isFALSE(blockfld))
     {
-        keys=ids
+        keys=ids   # ist das überhaupt nötig ?
         pairs=list()
+        if (!is.list(blockfld))
+            blockfld=list(blockfld)
         
         for (blockelem in blockfld)
         {
-          blockstr=apply(dataset[,blockelem,drop=F],1,paste,collapse=" ")
-          names(keys)=blockstr
+#           blockstr=apply(dataset[,blockelem,drop=F],1,paste,collapse="")
+#           names(keys)=blockstr
           
-          blocktable=list()
+          blocktable=list() # Liste: Blockingstring -> Vektor von Indizes mit
+                            # diesem Blockingstring
           
           for (i in 1:ndata)
-          {
-              x=blocktable[[blockstr[i],exact=T]]
-              if (is.null(i))
-              {
-                  blocktable[[blockstr[i]]]=keys[i]
-              } else
-              {
-                  blocktable[[blockstr[i]]]=c(x,keys[i])
-              }
+          { 
+              blockstr=paste(dataset[i,blockelem],collapse="")
+              x=blocktable[[blockstr,exact=T]]
+              blocktable[[blockstr]]=c(x,keys[i])
           }
-      
-      
+         # blocktable ist jetzt gefüllt
+                  
           for (e in blocktable)
           {
               if (length(e) > 1)
@@ -112,120 +134,36 @@ compare <- function(dataset, blockfld=FALSE, blockfun=NULL,ids=1:nrow(dataset),p
         pairs=t(pairs)
     } else # blockfld==FALSE
     {
-        pairs=t(ordered_pairs(ndata))
+        pairs=t(ordered_pairs(ids))
     }    
 
     if (isTRUE(strcmp))
     {
-       patterns=unique(cbind(pairs,blockfun(dataset[pairs[,1],,drop=F],
+       patterns=unique(cbind(pairs,strcmpfun(dataset[pairs[,1],,drop=F],
                                                dataset[pairs[,2],,drop=F])))
     } else 
     {
         patterns=unique(cbind(pairs,dataset[pairs[,1],,drop=F]==dataset[pairs[,2],,drop=F]))
         if (!identical(strcmp,F))
-            patterns[,strcmp+2]=blockfun(dataset[pairs[,1],strcmp,drop=F],
+            patterns[,strcmp+2]=strcmpfun(dataset[pairs[,1],strcmp,drop=F],
                                                dataset[pairs[,2],strcmp,drop=F])
     }                    
     
     if (nrow(patterns)>1)
-        patterns=patterns[order(patterns[,1],patterns[,2]),]
+        patterns=patterns[order(patterns[,1],patterns[,2]),] # muss nicht unbedingt sein, evtl Argument
+    patterns=as.data.frame(patterns)
+        patterns[,1:2]=cbind(ext_ids[patterns[,1]],ext_ids[patterns[,2]])
     colnames(patterns)=c("id1","id2",colnames(dataset))
     rownames(patterns)=NULL
-    return(patterns)
+    # berechne Häufigkeiten für die Ausgabe
+    frequencies=apply(dataset,2,function(x) 1/length(unique(x)))
+    ret=list()
+    ret$data=cbind(ids,dataset)
+    ret$pairs=patterns
+    ret$frequencies=frequencies
+    class(ret)="RecLinkPairs"
+    return(ret)
 }
-
-
-# erste Version der Funktion: quadratische Laufzeit, für realistische Längen
-# von dataset deutlich langsamer
-
-# compare <- function(dataset, blockfld=FALSE, blockfun=NULL,ids=1:nrow(dataset),phonetic=FALSE,strcmp=FALSE)
-# {
-#     ids # Argument wird erst initialisiert, wenn es gebraucht wird,
-#         # deshalb muss er hier auftauchen, bevor dataset transponiert wird 
-#     # format conversions
-#     dataset=t(as.matrix(dataset))     
-#     dataset[dataset==""]=NA
-#       
-#     ndata=ncol(dataset)
-# 
-#     if (!identical(blockfld,F))
-#     {
-#         if (!is.list(blockfld))
-#             blockfld=list(blockfld)
-#     
-#         outerexpr=character()    
-#         for (blockel in blockfld)
-#         {
-#             innerexpr=character()
-#             for (fld in blockel)
-#             {
-#                 innerexpr=append(innerexpr, sprintf("X[%d,]==1",fld+2))
-#             }
-#             outerexpr=append(outerexpr, paste("(",paste(innerexpr,collapse=" & "),")",sep=""))
-#         }
-#         expr=paste(outerexpr,collapse=" | ")
-# #        print(expr)
-#         
-#         eval(parse(text=sprintf("blockfun <- function(X) return(X[,%s,drop=F])",expr))) 
-#     
-#     }
-# 
-#     if (length(ids)!=ndata)
-#     {
-#        if (length(ids)!=1)
-#        {
-#           stop("length(ids) must be either nrow(data) or 1")
-#        }
-#        idcol=ids
-#        ids=dataset[,ids]
-#        dataset[[idcol]]=NULL
-#     } else
-# #        if (!identical(blockfld,F))
-# #           blockfld=lapply(blockfld,"+",2) # muss noch besser gemacht werden! 
-# 
-# 
-# #  if (phonetic)... erst Phonetik in R implementieren
-# 
-#     patterns=list()
-#     for (i in 1:(ndata-1))
-#     {        
-#         temp_patterns=rbind(ids[i],ids[(i+1):ndata],
-#             (dataset[,i]==dataset[,(i+1):ndata,drop=F]))
-#         if (!identical(blockfld,F))
-#         {
-#               temp_ids=blockfun(temp_patterns)[1:2,,drop=F]          
-#             if (ncol(temp_ids)==0)
-#                 next
-#         } else
-#         {
-#             temp_ids=temp_patterns[1:2,,drop=F]
-#             if (ncol(temp_ids)==0)
-#                 next
-#         }
-#             
-# 
-#         if (isTRUE(strcmp))
-#         {
-#             temp_patterns=rbind(temp_ids,
-#                 apply(dataset[,temp_ids[2,],drop=F],2,jarowinkler,dataset[,i]))
-#         } else
-#         {       
-#             temp_patterns=rbind(temp_ids,
-#                 (dataset[,i]==dataset[,temp_ids[2,],drop=F]))
-#             if (!identical(strcmp,F))
-#                 temp_patterns[strcmp+2,]=apply(dataset[strcmp,temp_ids[2,],drop=F],2,jarowinkler,dataset[strcmp,i])
-#          }     
-#         patterns[[i]]=temp_patterns
-#     }
-#     if (length(patterns)==0)
-#         return (NULL)
-#     # convert back to matrix
-#     patterns=unlist(patterns)
-#     patterns=unique(matrix(patterns,ncol=nrow(dataset)+2,nrow=length(patterns)/(nrow(dataset)+2),byrow=T))
-#     colnames(patterns)=c("id1","id2",rownames(dataset))
-#     return(patterns)
-# }
-
 
 
  
