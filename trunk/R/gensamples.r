1f3b14e9bb635e4b35e3fccafa4a8db53723387a
sariyar <sariyar@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-16 10:31:04 +0000

    Statt IDs werden ret Paare als eval, link und nonlink übergeben
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recordlinkage@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/gensamples.r
+++ trunk/R/gensamples.r
@@ -26,25 +26,22 @@
 
 library(e1071)
 library(mygllm)
-gensamples = function (datapairs, num_non, des_prop=0.1, nclasses=2, seed=10)
+# library(RecordLinkage) necessary for procedures involved
+gensamples = function (datapairs, num_non, des_prob=0.1, seed=10)
 {   
     set.seed(seed)
-    ngesamt=0
     ret=datapairs   
-    dmat=as.matrix(ret$pairs) # any filters will be set
-    ids=dmat[,c(1:2)]
-    # delete ids if existing; it exists, so delete the if sequence
-    dpairs=dmat[,-c(1:2)] 
+    ids=as.matrix(ret$pairs[,c(1:2)]) # any filters will be set
     #  number of classes is 3, when undetermindes cases are allowed
     # if (undet) nclasses=nclasses+1
     #  consistence checking
-    nlink=des_prop*(num_non) #+cler_review)
+    nlink=des_prob*(num_non) #+cler_review)
     ngesamt=num_non+nlink # +cler_review
     ndata=nrow(ids)
     if (ngesamt > ndata){stop("Inconsistent values for training data!")}
     # run clustering algorithm, Choosing?
-    # res_clust <- bclust(x=dmat, centers=3,base.center=2, base.method="kmeans", iter.base=100, verbose=T)
-    # kmendundet<-kmeans(x=dmat, centers=nclasses)
+    # res_clust <- bclust(x=ddmat, centers=3,base.center=2, base.method="kmeans", iter.base=100, verbose=T)
+    # kmendundet<-kmeans(x=ddmat, centers=nclasses)
     # pairs$pairs[kclusr$cluster==2,]
     # Use EM algorithm instead    
     ddpairs<-emWeights(ret)
@@ -57,8 +54,6 @@ gensamples = function (datapairs, num_non, des_prop=0.1, nclasses=2, seed=10)
     if(nlink > alidn) { warning("Only ", alidn, " Links!"); nlink=alidn }
     if(num_non > anolidn) { warning("Only ", anolidn, " Non-Links!"); num_non=anolidn }
     # Assumption: only two classes, then draw samples
-
-# kann man nicht auch mit Zeilennummern der pairs arbeiten? spart merge!
     salid<-sample(x=1:alidn, size=nlink)
     sanolid<-sample(x=1:anolidn, size=num_non)
     slinksid=merge(salid,cbind(1:alidn,linksid),by.x=1,by.y=1,sort=F)[,-1]
@@ -66,21 +61,24 @@ gensamples = function (datapairs, num_non, des_prop=0.1, nclasses=2, seed=10)
     trainallid=rbind(slinksid, snonlinksid)
     help1=paste(trainallid[,1],trainallid[,2])
     help2=paste(ids[,1],ids[,2])
-    trainhelp=help2 %in% help1 # T, falls Paar in Trainingsmenge ist
-    comptrainhelp=!trainhelp   # Komplement
-    help3=paste(slinksid[,1],slinksid[,2])
-    linkhelp=help2 %in% help3  # T, falls Paar Links ist und in Trainingsmenge
-    help4=paste(snonlinksid[,1],snonlinksid[,2])
-    nonlinkhelp=help2 %in% help4 # T, falls Paar Non-Link ist und in Trainingsmenge
+    trainhelp=help2 %in% help1
+    comptrainhelp=!trainhelp
+    
+    #help3=paste(slinksid[,1],slinksid[,2])
+    #linkhelp=help2 %in% help3
+    #help4=paste(snonlinksid[,1],snonlinksid[,2])
+    #nonlinkhelp=help2 %in% help4
     #ret$evals=ret$pairs[trainhelp,]
-    ret$evalids=comptrainhelp
-    ret$linkids=linkhelp
-    ret$nonlinkids=nonlinkhelp
-    #slinks=merge(slinksid, ret$pairs, by.x=c(1,2), by.y=c(1,2),sort=F)
-    #snonlinks=merge(snonlinksid, ret$pairs, by.x=c(1,2), by.y=c(1,2),sort=F)
-    # trainall=merge(trainallid, ret$pairs, by.x=c(1,2), by.y=c(1,2),sort=F)
-    #ret$slinks=slinks
-    #ret$snonlinks=snonlinks
+    #ret$evalids=comptrainhelp
+    #ret$linkids=linkhelp
+    #ret$nonlinkids=nonlinkhelp
+    
+    slinks=merge(slinksid, ret$pairs, by.x=c(1,2), by.y=c(1,2),sort=F)
+    snonlinks=merge(snonlinksid, ret$pairs, by.x=c(1,2), by.y=c(1,2),sort=F)
+    trainall=merge(trainallid, ret$pairs, by.x=c(1,2), by.y=c(1,2),sort=F)
+    ret$slinks=slinks
+    ret$snonlinks=snonlinks
+    ret$evals=ret$pairs[comptrainhelp,]
     class(ret)="RecLinkPairs"
     return(ret)
 }
