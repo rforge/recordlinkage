4a5c226799b96eb80679afd74fb69a42b5e1a5d6
anborg <anborg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-22 14:00:58 +0000

    added RSQLite extension function for Jaro-Winkler
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recordlinkage@165 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/devel/src/sqlite_extensions.c
@@ -0,0 +1,57 @@
+/* Prototyp zum Laden von Erweiterungen in SQLite mittels Funktion 
+   load_extension() 
+   Informationen dazu: http://www.sqlite.org/cvstrac/wiki?p=LoadableExtensions
+   In Datei DESCRIPTION des Pakets muss "LinkingTo: RSQLite" benutzt werden
+   */
+
+
+#include "sqlite3ext.h"
+#include <string.h>
+
+SQLITE_EXTENSION_INIT1
+
+
+double jarowinkler_core(char * str_1, char * str_2,
+             double W_1, double W_2, double W_t,
+             double r);
+
+
+
+void jarowinkler_wrapper(sqlite3_context *ctx, int n_values, sqlite3_value **value)
+{
+  const unsigned char *str1 = sqlite3_value_text(value[0]);
+  const unsigned char *str2 = sqlite3_value_text(value[1]);
+//   Rprintf("String 1: %s\n", str1);
+//   Rprintf("String 2: %s\n", str2);
+  double result;
+  result = jarowinkler_core(str1, str2, 1.0/3, 1.0/3, 1.0/3, 0.5);
+//   Rprintf("Ergebnis des Stringvergleichs: %f", result);
+  sqlite3_result_double(ctx, result);
+}
+
+
+/* SQLite invokes this routine once when it loads the extension.
+** Create new functions, collating sequences, and virtual table
+** modules here.  This is usually the only exported symbol in
+** the shared library.
+*/
+int sqlite3_extension_init(
+  sqlite3 *db_connection,
+  char **pzErrMsg,
+  const sqlite3_api_routines *pApi
+){
+  SQLITE_EXTENSION_INIT2(pApi)
+
+   sqlite3_create_function(
+      db_connection,
+      "jarowinkler",
+      2,
+      SQLITE_UTF8,
+      NULL,
+      &jarowinkler_wrapper,
+      NULL,
+      NULL
+      );
+  return 0;
+}
+      
