e4a076af45c6d009f7c961984055fe58686e8ac8
anborg <anborg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-24 08:55:35 +0000

    implemented SQLite extension for phonetic code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recordlinkage@170 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/devel/src/sqlite_extensions.c
+++ branches/devel/src/sqlite_extensions.c
@@ -7,16 +7,29 @@
 
 #include "sqlite3ext.h"
 #include <string.h>
+#include "R.h"
+
 
 SQLITE_EXTENSION_INIT1
 
+
+#define DEBUG
+#ifndef max
+	#define max( a, b ) ( ((a) > (b)) ? (a) : (b) )
+#endif
+/* Prototypes of functions found in other source files */
 
 double jarowinkler_core(char * str_1, char * str_2,
              double W_1, double W_2, double W_t,
              double r);
 
+int levenshtein_internal(const char *s, const char *t,
+                     int ins_c, int del_c, int sub_c);
+
+int phonet (char src[], char dest[], int len, int mode);
 
 
+/* wrapper for Jaro-Winkler string comparison */
 void jarowinkler_wrapper(sqlite3_context *ctx, int n_values, sqlite3_value **value)
 {
 	// check for NULL values, return NULL if any of the input strings is NULL
@@ -28,14 +41,76 @@ void jarowinkler_wrapper(sqlite3_context *ctx, int n_values, sqlite3_value **val
 	}
   const unsigned char *str1 = sqlite3_value_text(value[0]);
   const unsigned char *str2 = sqlite3_value_text(value[1]);
-//   Rprintf("String 1: %s\n", str1);
-//   Rprintf("String 2: %s\n", str2);
+	#ifdef DEBUG
+ 	  Rprintf("String 1: %s\n", str1);
+		Rprintf("String 2: %s\n", str2);
+	#endif
   double result;
   result = jarowinkler_core(str1, str2, 1.0/3, 1.0/3, 1.0/3, 0.5);
-//   Rprintf("Ergebnis des Stringvergleichs: %f", result);
+	#ifdef DEBUG
+  	Rprintf("Ergebnis des Stringvergleichs: %f", result);
+  #endif
   sqlite3_result_double(ctx, result);
 }
 
+/* wrapper for Levenshtein string comparison */
+void levenshtein_wrapper(sqlite3_context *ctx, int n_values, sqlite3_value **value)
+{
+	// check for NULL values, return NULL if any of the input strings is NULL
+	if(sqlite3_value_type(value[0]) == SQLITE_NULL || 
+  	 sqlite3_value_type(value[1]) == SQLITE_NULL)
+  {
+		sqlite3_result_null(ctx);
+		return;
+	}
+  const unsigned char *str1 = sqlite3_value_text(value[0]);
+  const unsigned char *str2 = sqlite3_value_text(value[1]);
+	#ifdef DEBUG
+  	Rprintf("String 1: %s\n", str1);
+		Rprintf("String 2: %s\n", str2);
+	#endif
+  double result;
+  int editDistance;
+  editDistance = levenshtein_internal(str1, str2, 1, 1, 1);
+  /* Only the string metric based on the edit distance is used in this
+     package, therefore transform right here */
+  result = 1.0 - (double) editDistance / (double) max(strlen(str1), strlen(str2));
+	#ifdef DEBUG
+		Rprintf("Ergebnis des Stringvergleichs: %f", result);
+	#endif
+  sqlite3_result_double(ctx, result);
+}
+
+void pho_h_wrapper(sqlite3_context *ctx, int n_values, sqlite3_value **value)
+{
+	// check for NULL values, return NULL if input string is NULL
+	if(sqlite3_value_type(value[0]) == SQLITE_NULL)
+  {
+		sqlite3_result_null(ctx);
+		return;
+	}
+	
+  const unsigned char *str1 = sqlite3_value_text(value[0]);
+  int str1len = strlen(str1) + 1; // save string length with(!) delimiter
+	char *dest = (char*) R_alloc(sizeof(char), str1len);
+	#ifdef DEBUG
+  	Rprintf("String: %s\n", str1);
+	#endif
+	
+	int result;
+  result = phonet(str1, dest, str1len, 1);
+	/* throw error if phonet fails (result <0) */
+	if (result < 0)
+	{
+		sqlite3_result_error(ctx, "phonet() terminated with an error", -1);
+		return;
+	}
+	#ifdef DEBUG
+		Rprintf("Ergebnis von phonet(): %s", dest);
+	#endif
+	
+	sqlite3_result_text(ctx, dest, -1, SQLITE_STATIC);
+}
 
 /* SQLite invokes this routine once when it loads the extension.
 ** Create new functions, collating sequences, and virtual table
@@ -56,6 +131,27 @@ int sqlite3_extension_init(
       SQLITE_UTF8,
       NULL,
       &jarowinkler_wrapper,
+      NULL,
+      NULL
+      );
+   
+	 sqlite3_create_function(
+      db_connection,
+      "levenshtein",
+      2,
+      SQLITE_UTF8,
+      NULL,
+      &levenshtein_wrapper,
+      NULL,
+      NULL
+      );
+	 sqlite3_create_function(
+      db_connection,
+      "pho_h",
+      1,
+      SQLITE_UTF8,
+      NULL,
+      &pho_h_wrapper,
       NULL,
       NULL
       );
