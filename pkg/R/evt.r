3cf81598cf8f1803908eb34d4559119b7134273d
anborg <anborg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-10-25 15:38:14 +0000

    Added check if bringToTop is available
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/recordlinkage@421 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/evt.r
+++ pkg/R/evt.r
@@ -145,7 +145,7 @@ setMethod(
         message("No data in selected range! Choose a larger interval.")
         flush.console()
       }
-      bringToTop(-1)
+      if (existsFunction("bringToTop")) bringToTop(-1)
       if (length(indices)==0)
         stop("At least the left endpoint of the interval must be chosen!")
     }
@@ -188,7 +188,7 @@ setMethod(
       {
         plotMRL(NULL,l=l)
   #      title(main=rpairs$description)
-        bringToTop()
+        if (existsFunction("bringToTop")) bringToTop()
         indices=sort(identify(l$x,l$y,n=2,labels=signif(l$x,4)))
         interval=l$x[indices]
         if (length(indices)==0)
@@ -211,7 +211,7 @@ setMethod(
         message("No data in selected range! Choose a larger interval.")
         flush.console()
       }
-      bringToTop(-1)
+      if (existsFunction("bringToTop")) bringToTop(-1)
       if (length(indices)==0)
         stop("At least the left endpoint of the interval must be chosen!")
     }
